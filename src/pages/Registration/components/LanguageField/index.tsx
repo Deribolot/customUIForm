c47c460ce27b8ee0ae5b662a5f18c60eaeadb524
YDeribolot <yuliya.deribolot@zyfra.com>
2022-05-05 23:18:38 +0300

    Добавление действий клавиатуры и доступности выпадающему списку на radio

--- src/pages/Registration/components/LanguageField/index.tsx
+++ src/pages/Registration/components/LanguageField/index.tsx
@@ -1,23 +1,15 @@
 import React, { useMemo, useCallback, useState } from 'react';
 import classnames from 'classnames';
+import forEach from 'lodash/forEach';
+import map from 'lodash/map';
 import { useIntl } from '@@/plugin-locale/localeExports';
 import styles from './index.less';
+
+const languages = ['ru', 'en', 'zh', 'es'];
 
 const LanguageField: React.FC = function LanguageField() {
   const intl = useIntl();
 
-  const languageRuTitle = intl.formatMessage({
-    id: 'app.page.registration.field.language.option.ru',
-  });
-  const languageEnTitle = intl.formatMessage({
-    id: 'app.page.registration.field.language.option.en',
-  });
-  const languageZhTitle = intl.formatMessage({
-    id: 'app.page.registration.field.language.option.zh',
-  });
-  const languageEsTitle = intl.formatMessage({
-    id: 'app.page.registration.field.language.option.es',
-  });
   const languageNoneTitle = intl.formatMessage({
     id: 'app.page.registration.field.language.placeholder',
   });
@@ -47,45 +39,57 @@ const LanguageField: React.FC = function LanguageField() {
     [value],
   );
 
-  const handleValueChange = useCallback<(e: React.FormEvent<HTMLInputElement>) => void
-  >((e) => {
-      const target = e.target as HTMLInputElement;
-      setValue(target.value);
-    }, []);
+  const handleValueChange = useCallback<(e: React.FormEvent<HTMLInputElement>) => void>((e) => {
+    const target = e.target as HTMLInputElement;
+    setValue(target.value);
+  }, []);
+
+  const handleValueKeyUp = useCallback<(e: React.KeyboardEvent<HTMLInputElement>) => void>((e) => {
+    const { length } = languages;
+
+    let index = -1;
+    forEach(languages, (v, i) => {
+      if (value === v) {
+        index = i;
+      }
+    });
+
+    if (e.code === 'ArrowDown' && index < length - 1) {
+      setValue(languages[index + 1]);
+    }
+
+    if (e.code === 'ArrowUp' && index > 0) {
+      setValue(languages[index - 1]);
+    }
+  }, [value]);
 
   return (
-    <label htmlFor="language">
+    <label htmlFor="language" id="language-label">
       {intl.formatMessage({ id: 'app.page.registration.field.language.label' })}
-      <details className={detailsClassname} onChange={handleValueChange}>
-        <summary className={styles.summary}>
-          <input
-            type="radio"
-            name="language"
-            id="language-ru"
-            value="ru"
-            title={languageRuTitle}
-          />
-          <input
-            type="radio"
-            name="language"
-            id="language-en"
-            value="en"
-            title={languageEnTitle}
-          />
-          <input
-            type="radio"
-            name="language"
-            id="language-zh"
-            value="zh"
-            title={languageZhTitle}
-          />
-          <input
-            type="radio"
-            name="language"
-            id="language-es"
-            value="es"
-            title={languageEsTitle}
-          />
+      <details
+        className={detailsClassname}
+        onChange={handleValueChange}
+        role="listbox"
+        onKeyUp={handleValueKeyUp}
+      >
+        <summary
+          className={styles.summary}
+          aria-labelledby={`language-label${value ? ` language-${value}` : ''}`}
+        >
+          {map(languages, (lang) => (
+            <input
+              key={lang}
+              type="radio"
+              name="language"
+              id={`language-${lang}`}
+              value={lang}
+              title={intl.formatMessage({
+                id: `app.page.registration.field.language.option.${lang}`,
+              })}
+              role="presentation"
+              checked={value === lang}
+            />
+          ))}
           <input
             type="radio"
             name="language"
@@ -93,29 +97,19 @@ const LanguageField: React.FC = function LanguageField() {
             value=""
             title={languageNoneTitle}
             defaultChecked
+            role="presentation"
           />
         </summary>
-        <ul className={styles.list}>
-          <li>
-            <label htmlFor="language-ru" className={labelClassNames.ru}>
-              {languageRuTitle}
-            </label>
-          </li>
-          <li>
-            <label htmlFor="language-en" className={labelClassNames.en}>
-              {languageEnTitle}
-            </label>
-          </li>
-          <li>
-            <label htmlFor="language-zh" className={labelClassNames.zh}>
-              {languageZhTitle}
-            </label>
-          </li>
-          <li>
-            <label htmlFor="language-es" className={labelClassNames.es}>
-              {languageEsTitle}
-            </label>
-          </li>
+        <ul className={styles.list} role="presentation">
+          {map(languages, (lang) => (
+            <li role="option" aria-selected={value === lang} key={lang}>
+              <label htmlFor="language-ru" className={labelClassNames[lang]}>
+                {intl.formatMessage({
+                  id: `app.page.registration.field.language.option.${lang}`,
+                })}
+              </label>
+            </li>
+          ))}
         </ul>
       </details>
     </label>
